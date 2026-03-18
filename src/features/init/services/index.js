249968a943288024e123d288d44a787fcafcee4c
flodus <ffaucheux@gmail.com>
2026-03-18 21:03:59 +0100

    refactor: supprime doublon features/init/services/countryValidation.js

--- src/features/init/services/index.js
+++ src/features/init/services/index.js
@@ -1,3 +1,3 @@
 export { getTerrainLabels, getRegimeLabels, getPaysLocaux } from './labels';
-export { rcMatch, rcDisplayName, getAllCountries, validateCountryWithAI } from './countryValidation';
+export { rcMatch, rcDisplayName, getAllCountries, validateCountryWithAI } from '../../../shared/services/country';
 export { getRealCountries } from './realCountries';
